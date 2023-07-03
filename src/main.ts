bce71fdc9615a5622b647964239a56862a0570f7
Ch1n3du <danielonyesoh@gmail.com>
2023-07-03 22:56:29 +0100

    fix: Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,17 @@ patchNestJsSwagger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
 
+  // Setup swagger docs
   patchNestJsSwagger();
-  const swaggerOptions: SwaggerDocumentOptions = {
-    // deepScanRoutes: true,
-  };
   const config = new DocumentBuilder()
     .setTitle('Realworld Backend')
     .setDescription('Realworld API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config, swaggerOptions);
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(8080);
